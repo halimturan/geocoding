9f369d4f0d43b3a63cae368e3f5eaf56d58dd1a4
Halim Turan <halim.turan>
2022-02-01 14:57:19 +0300

    poi eklendi

--- api/views/yol.py
+++ api/views/yol.py
@@ -1,16 +1,26 @@
-from rest_framework import viewsets, filters
+from rest_framework import viewsets
+from rest_framework_gis.filterset import GeoFilterSet
+from rest_framework_gis.filters import GeometryFilter
 from api.serializer import YolSerializer, YolGeojsonSerializer
 from api.models import Yol
 from rest_framework.decorators import action
 from rest_framework.response import Response
-from django_filters.rest_framework import DjangoFilterBackend
+from django_filters.filters import CharFilter
+
+
+class RegionFilter(GeoFilterSet):
+    id = CharFilter(field_name="id")
+    contains_geom = GeometryFilter(field_name="geo", lookup_expr='contains')
+
+    class Meta:
+        exclude = ('geo',)
+        model = Yol
 
 
 class YolViewSet(viewsets.ModelViewSet):
     queryset = Yol.objects.all()
     serializer_class = YolSerializer
-    filter_backends = (DjangoFilterBackend, filters.OrderingFilter,)
-    filterset_fields = ('id',)
+    filter_class = RegionFilter
 
     @action(detail=False)
     def type_geojson(self, request, *args, **kwargs):
