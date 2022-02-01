d57b1f431f23c590bce047210f96c02230a72e59
Halim Turan <halim.turan>
2022-02-01 15:30:35 +0300

    poi eklendi

--- api/views/kapi.py
+++ api/views/kapi.py
@@ -1,9 +1,8 @@
-from rest_framework import viewsets, filters
+from rest_framework import viewsets
 from api.serializer import KapiSerializer, KapiGeojsonSerializer
 from api.models import Kapi
 from rest_framework.decorators import action
 from rest_framework.response import Response
-from django_filters.rest_framework import DjangoFilterBackend
 from django_filters.filters import CharFilter
 from rest_framework_gis.filterset import GeoFilterSet
 from rest_framework_gis.filters import GeometryFilter
@@ -21,8 +20,7 @@ class RegionFilter(GeoFilterSet):
 class KapiViewSet(viewsets.ModelViewSet):
     queryset = Kapi.objects.all()
     serializer_class = KapiSerializer
-    filter_backends = (DjangoFilterBackend, filters.OrderingFilter,)
-    filterset_fields = ('id',)
+    filter_class = RegionFilter
 
     @action(detail=False)
     def type_geojson(self, request, *args, **kwargs):
--- api/views/mahalle.py
+++ api/views/mahalle.py
@@ -1,9 +1,8 @@
-from rest_framework import viewsets, filters
+from rest_framework import viewsets
 from api.serializer import MahalleSerializer, MahalleGeojsonSerializer
 from api.models import Mahalle
 from rest_framework.decorators import action
 from rest_framework.response import Response
-from django_filters.rest_framework import DjangoFilterBackend
 from django_filters.filters import CharFilter
 from rest_framework_gis.filterset import GeoFilterSet
 from rest_framework_gis.filters import GeometryFilter
@@ -21,8 +20,7 @@ class RegionFilter(GeoFilterSet):
 class MahalleViewSet(viewsets.ModelViewSet):
     queryset = Mahalle.objects.all()
     serializer_class = MahalleSerializer
-    filter_backends = (DjangoFilterBackend, filters.OrderingFilter,)
-    filterset_fields = ('id',)
+    filter_class = RegionFilter
 
     @action(detail=False)
     def type_geojson(self, request, *args, **kwargs):
