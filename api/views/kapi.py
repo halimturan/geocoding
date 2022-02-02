33b120468c2adb3ec7eec97f36ce6db24836c4b4
Halim Turan <halim.turan>
2022-02-02 10:01:59 +0300

    poi eklendi

--- api/views/kapi.py
+++ api/views/kapi.py
@@ -3,24 +3,14 @@ from api.serializer import KapiSerializer, KapiGeojsonSerializer
 from api.models import Kapi
 from rest_framework.decorators import action
 from rest_framework.response import Response
-from django_filters.filters import CharFilter
-from rest_framework_gis.filterset import GeoFilterSet
-from rest_framework_gis.filters import GeometryFilter
-
-
-class RegionFilter(GeoFilterSet):
-    id = CharFilter(field_name="id")
-    contains_geom = GeometryFilter(field_name="geo", lookup_expr='contains')
-
-    class Meta:
-        exclude = ('geo',)
-        model = Kapi
+from rest_framework_gis.filters import DistanceToPointFilter
 
 
 class KapiViewSet(viewsets.ModelViewSet):
     queryset = Kapi.objects.all()
     serializer_class = KapiSerializer
-    filter_class = RegionFilter
+    distance_filter_field = 'geo'
+    filter_backends = (DistanceToPointFilter,)
 
     @action(detail=False)
     def type_geojson(self, request, *args, **kwargs):
